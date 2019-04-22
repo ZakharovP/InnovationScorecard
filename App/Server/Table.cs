00fb7c09fc50fdb0681638415add1b8b6aa51975
ZakharovP <45815754+ZakharovP@users.noreply.github.com>
2019-04-22 14:29:45 +0300

    Starting Table implemetation

---
+++ App/Server/Table.cs
@@ -0,0 +1,26 @@
+﻿using Lib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App.Server
+{
+    public class Table
+    {
+        public DataTable ParamTable { private set; get; }
+        public DataTable ScoreTable { private set; get; }
+        /// <summary>
+        /// Dictionary countries contains mapping of country name to country object
+        /// </summary>
+        private Dictionary<string, Country> countries = new Dictionary<string, Country>();
+        public Table(DataTable paramTable, DataTable scoreTable)
+        {
+            ParamTable = paramTable;
+            ScoreTable = scoreTable;
+        }
+    }
+}
